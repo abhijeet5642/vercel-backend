edbbd6d7df47e16e7eb0788122a5c43d5ded3f15
abhijeet <aydv760@gmail.com>
2025-11-18 19:38:14 +0530

    campuseventhub

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(process.env.DB_CONNECT);
+        await mongoose.connect(process.env.DB_CONNECT || 'mongodb+srv://aydv760:1its8eG1SjDl53zN@abhijeet.2gj2pel.mongodb.net/campus?appName=abhijeet');
         console.log('Connected to MongoDB successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
